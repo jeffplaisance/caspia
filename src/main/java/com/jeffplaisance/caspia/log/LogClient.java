ebd335e88024588b4c9a20e7799b9b696eea410f
Jeff Plaisance <jeffplaisance@gmail.com>
2020-07-04 12:23:32 -0500

    mostly refactoring and comments. added flags to disable fast path optimization and one round trip read optimization.

--- src/main/java/com/jeffplaisance/caspia/log/LogClient.java
+++ src/main/java/com/jeffplaisance/caspia/log/LogClient.java
@@ -10,6 +10,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.annotation.Nullable;
+import javax.annotation.concurrent.NotThreadSafe;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Comparator;
@@ -17,10 +18,18 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+@NotThreadSafe
 public final class LogClient {
 
     private static final Logger LOG = LoggerFactory.getLogger(LogClient.class);
     private static final Comparator<LogReplicaResponse> MAX_ACCEPTED = Ordering.from((LogReplicaResponse a, LogReplicaResponse b) -> Ints.compare(a.getAccepted(), b.getAccepted())).nullsFirst();
+    private static final boolean fastPathEnabled;
+    private static final boolean oneRoundTripReadsEnabled;
+
+    static {
+        fastPathEnabled = !Boolean.getBoolean("com.jeffplaisance.caspia.disableFastPath");
+        oneRoundTripReadsEnabled = !Boolean.getBoolean("com.jeffplaisance.caspia.disableOneRoundTripRead");
+    }
 
     private final List<LogReplicaClient> replicas;
     private final int n;
@@ -34,29 +43,25 @@ public final class LogClient {
         this.replicas = new ArrayList<>(replicas);
     }
 
-    // a return value of true means that value was committed at index
-    // a return value of false means that a value was already committed at index (could possibly be equal to value but it was already there)
-    // an IOException is thrown if less than a quorum of responses is obtained (can be triggered by a conflicting client)
-    // this method does not retry on failures or conflicts. retry logic should be handled by the caller.
-
+    /**
+     * @param index the index
+     * @param value the value that we will be attempting to write at index
+     * @return a return value of true means that value was committed at index. a return value of false means that a
+     * value was already committed at index. it is possible for the committed value to be binary equivalent to value and
+     * for this method to still return false if the same value had been proposed and accepted in an earlier round.
+     * @throws Exception an IOException is thrown if less than a quorum of responses is obtained (can be triggered by a
+     * conflicting client). this method does not retry on failures or conflicts. retry logic should be handled by the caller.
+     */
     public boolean write(long index, byte[] value) throws Exception {
-        // null is used as a sentinel value to signify that no value has been written at this index yet
-        // it is valid for there to be a null accepted value which is allowed to transition to a real value later
-        // accepted non-null values can never be changed
+        // null is used as a sentinel value to signify that no value has been written at this index yet.
+        // it is valid for there to be a null accepted value which will transition to a non-null value later.
+        // accepted non-null values can never be changed.
         Preconditions.checkNotNull(value);
+        Preconditions.checkArgument(index > 0);
 
-        // this is true iff this client was the one to commit a non-null value at index-1
-        // fast path always uses proposal number 1
-        // any competing proposer will start at proposal number 2 and block fast path
-        if (index == fastPathIndex) {
-            final List<Boolean> responses = Quorum.broadcast(replicas, n-f, replica -> replica.writeAtomic(index, 1, 1, value, true, 0, 0), false);
-            if (Base.sum(responses) >= n-f) {
-                fastPathIndex = index+1;
-                return true;
-            }
-        }
+        if (tryFastPathWrite(index, value)) return true;
 
-        final List<LogReplicaResponse> initialValues = Quorum.broadcast(replicas, n-f, replica -> replica.read(index), LogReplicaResponse.EMPTY);
+        final List<LogReplicaResponse> initialValues = readInitialValues(index);
         // reference equality check is intentional
         if (write2(index, value, initialValues) == value) {
             fastPathIndex = index+1;
@@ -66,11 +71,54 @@ public final class LogClient {
         return false;
     }
 
-    @Nullable
-    private byte[] write2(final long index, final byte[] value, final List<LogReplicaResponse> initialValues) throws Exception {
+    private boolean tryFastPathWrite(long index, byte[] value) throws Exception {
+        if (fastPathEnabled) {
+            // fast path is an optimization to allow one round trip writes
+            // this is true iff this client was the one to commit a non-null value at index-1
+            // fast path always uses proposal number 1
+            // any competing proposer will start at proposal number 2 and block fast path
+            if (index == fastPathIndex) {
+                final List<Boolean> responses = Quorum.broadcast(replicas, n - f, replica -> replica.writeAtomic(index, 1, 1, value, true, 0, 0), false);
+                if (Base.sum(responses) >= n - f) {
+                    fastPathIndex = index + 1;
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private List<LogReplicaResponse> readInitialValues(long index) throws Exception {
+        return Quorum.broadcast(replicas, n - f, replica -> replica.read(index), LogReplicaResponse.EMPTY);
+    }
+
+    /**
+     * this function is the core of the algorithm. chooses next proposal number based on initial values and runs
+     * propose and accept phases of the algorithm.
+     * @param index the index
+     * @param value the value to write at index
+     * @param initialValues the state of the rows at index read from a quorum of replicas. response should be
+     * LogReplicaResponse.EMPTY for replicas from which we did not receive a response.
+     * @return the value that was written. reference equality of return value and input value is guaranteed if
+     * input value was the value written.
+     * @throws Exception throws IOException if successful on less than a quorum or replicas
+     */
+    private @Nullable byte[] write2(final long index, final @Nullable byte[] value, final List<LogReplicaResponse> initialValues) throws Exception {
 
         // lowest possible value for newProposal is 2 since 1 is reserved for fast path
         final int newProposal = initialValues.stream().map(LogReplicaResponse::getProposal).reduce(1, Math::max)+1;
+        final List<Boolean> proposeResponses = doPropose(index, initialValues, newProposal);
+        return doAccept(index, value, initialValues, newProposal, proposeResponses);
+    }
+
+    /**
+     * @param index the index
+     * @param initialValues the initial values
+     * @param newProposal the proposal number to try, must be greater than all proposal numbers in initialValues
+     * @return list of booleans indexed on replica corresponding to whether or not propose succeeded on that replica
+     * @throws Exception throws IOException if successful on less than a quorum or replicas
+     */
+    private List<Boolean> doPropose(long index, List<LogReplicaResponse> initialValues, int newProposal) throws Exception {
         // attempt to increase proposal to newProposal on all replicas leaving all other fields the same
         final List<ThrowingFunction<LogReplicaClient, Boolean, Exception>> proposeFunctions = initialValues.stream()
                 .<ThrowingFunction<LogReplicaClient, Boolean, Exception>>map(
@@ -87,7 +135,21 @@ public final class LogClient {
 
         // check for success on a quorum of replicas, throw exception on failure
         if (Base.sum(proposeResponses) < n-f) throw new IOException();
+        return proposeResponses;
+    }
 
+    /**
+     * @param index the index
+     * @param value the value to attempt to write at index
+     * @param initialValues initial values
+     * @param newProposal the proposal number for which we successfully updated a quorum of replicas
+     * @param proposeResponses list of booleans indexed on replica corresponding to whether or not propose succeeded on
+     * that replica
+     * @return the value that was written. reference equality of return value and input value is guaranteed if input
+     * value was the value written.
+     * @throws Exception throws IOException if successful on less than a quorum or replicas
+     */
+    private @Nullable byte[] doAccept(long index, @Nullable byte[] value, List<LogReplicaResponse> initialValues, int newProposal, List<Boolean> proposeResponses) throws Exception {
         // find the response with the highest accepted number from the replicas where propose succeeded
         final LogReplicaResponse maxInitial = IntStream.range(0, initialValues.size())
                 .filter(proposeResponses::get)
@@ -118,40 +180,77 @@ public final class LogClient {
         return valueWritten;
     }
 
+    /**
+     * @param index the index
+     * @return the value written at the index or null if no value has been written at the index. a return value of null
+     * implies that the values at all indices greater than index are also null.
+     * @throws Exception an IOException is thrown if less than a quorum of responses is obtained (can be triggered by a
+     * conflicting client). this method does not retry on failures or conflicts. retry logic should be handled by the caller.
+     */
     @Nullable
     public byte[] read(long index) throws Exception {
-        final List<LogReplicaResponse> responses = Quorum.broadcast(replicas, n-f, replica -> replica.read(index), LogReplicaResponse.EMPTY);
-        final LogReplicaResponse maxAccepted = responses.stream().max(MAX_ACCEPTED).orElse(LogReplicaResponse.EMPTY);
-        final byte[] maxValue = maxAccepted.getValue();
-        if (maxAccepted.getAccepted() > 0 && maxValue != null) {
-            final long maxAcceptedCount = responses.stream().filter(r -> r.getAccepted() == maxAccepted.getAccepted()).count();
-            if (maxAcceptedCount >= n - f) {
-                // if there is a non-null value committed in the same round at a quorum of replicas it cannot change
-                // and we can return it safely.
-                return maxValue;
+        Preconditions.checkArgument(index > 0);
+
+        final List<LogReplicaResponse> responses = readInitialValues(index);
+
+        if (oneRoundTripReadsEnabled) {
+            // this is an optimization to allow one round trip reads at indexes which have a non-null committed value.
+            // this block is not required for correctness.
+            final LogReplicaResponse maxAccepted = responses.stream().max(MAX_ACCEPTED).orElse(LogReplicaResponse.EMPTY);
+            final byte[] maxValue = maxAccepted.getValue();
+            if (maxValue != null) {
+                final long maxAcceptedCount = responses.stream().filter(r -> r.getAccepted() == maxAccepted.getAccepted()).count();
+                if (maxAcceptedCount >= n - f) {
+                    // if there is a non-null value committed in the same round at a quorum of replicas it cannot change
+                    // and we can return it safely.
+                    return maxValue;
+                }
             }
         }
-        // we need to attempt to write the value with the highest accept round number to a quorum of replicas before we
-        // can be certain it is committed.
-        // if maxValue is null, in order to prevent linearizability issues we have to commit a null value before we can
-        // be certain that this index has not yet been written.
-        // this prevents partially committed values either through fast path or normal path from becoming fully
-        // committed by future read operations.
-        // this null value must be overwritten before writing to any higher index.
-        return write2(index, maxValue, responses);
+
+        // attempting to write null is the equivalent of writing a noop in multi paxos. we are able to take advantage
+        // of the ability to create a compare-and-swap register from CASPaxos to enable us to overwrite the null value
+        // later, which allows us to ensure a gap-free log. this null value must be overwritten before any non-null
+        // write occurs at index+1.
+        // writing on read is necessary to prevent linearizability issues that could otherwise arise due to the
+        // potential for partially committed values to become fully committed as a result of future read operations.
+        // if there is a partially committed value this will either force it to become fully committed or overwrite it
+        // with a null value.
+        return write2(index, null, responses);
     }
 
+    /**
+     * to find the end of the log, call this method and then call read in a loop starting from index and incrementing
+     * by one each iteration until a null value is found.
+     * @return reads the maximum index with a non-zero proposal from a quorum of replicas and returns the maximum of
+     * those indices.
+     * @throws Exception if less than a quorum of responses is obtained. this method does not retry on failures or
+     * conflicts. retry logic should be handled by the caller.
+     */
     public long readLastIndex() throws Exception {
         final List<Long> maxValues = Quorum.broadcast(replicas, n-f, LogReplicaClient::readLastIndex, 0L);
         return maxValues.stream().reduce(0L, Math::max);
     }
 
+    /**
+     * helper that calls read and parses the result as a UTF-8 string
+     * @param index the index
+     * @return the result of read converted to a string or null if the result of read was null
+     * @throws Exception if read throws an exception or if the value is not null or a valid UTF-8 string
+     */
     @Nullable
     public String readString(long index) throws Exception {
         final byte[] bytes = read(index);
         return bytes == null ? null : new String(bytes, Base.UTF_8);
     }
 
+    /**
+     * helper that converts string value to UTF-8 bytes and calls write
+     * @param index the index
+     * @param value the value to write at index
+     * @return the result of calling write at index with value converted to UTF-8 bytes
+     * @throws Exception if write throws an exception
+     */
     public boolean writeString(long index, String value) throws Exception {
         return write(index, value.getBytes(Base.UTF_8));
     }
